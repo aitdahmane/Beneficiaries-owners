da2dae7c6800fb703f2d8e037e29c6435287f8b6
Ayoub Ait Dahmane <dahmanicoder@gmail.com>
2025-03-12 04:47:59 +0100

    chore: init the project with basics dependencies

---
+++ src/main/java/com/bpifrance/beneficiariesmanagment/BeneficiariesmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.bpifrance.beneficiariesmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiariesmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiariesmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpifrance/beneficiariesmanagment/BeneficiariesmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpifrance.beneficiariesmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiariesmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
